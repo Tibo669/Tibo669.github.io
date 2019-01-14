f5ecd09f402108061f1bf1ebe6be048f7a0cfb28
Tibo669 <thib669@hotmail.fr>
2019-01-14 20:09:48 +0100

    Réajoute la carte !

--- src/js/main.js
+++ src/js/main.js
@@ -44,3 +44,14 @@ function pad(n) {
 <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
 window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
 ga('create', 'UA-XXXXX-Y', 'auto'); ga('send', 'pageview');
+
+
+document.getElementById('event-button').addEventListener('click',function(){
+  if(document.getElementById('event-field').value=='test'){
+    location.href='testevent.html';
+  }
+  else{
+    document.getElementById('error').innerHTML='Le mot de pass est incorecte';
+  }
+},false);
+
