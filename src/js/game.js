4a947a186b95f4a6dbeb0629c267f5da6ed150c8
tb6a210n <thibault-externe.besacier@enedis.fr>
2019-01-15 17:09:22 +0100

    Ecoute l'appuie sur la touche Entrée

--- src/js/game.js
+++ src/js/game.js
@@ -1,3 +1,25 @@
+(function () {
+  if ( typeof window.CustomEvent === "function" ) return false;
+
+  function CustomEvent ( event, params ) {
+    params = params || { bubbles: true, cancelable: true, detail: undefined };
+    let evt = document.createEvent('submit');
+    evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+    return evt;
+  }
+
+  CustomEvent.prototype = window.Event.prototype;
+
+  window.CustomEvent = CustomEvent;
+
+})();
+
+let evt = new CustomEvent("submit" );
+
+document.getElementById("eventField").addEventListener("submit",function(event) {
+  event.preventDefault();
+});
+
 document.getElementById('eventButton').addEventListener('click',function(){
   const event_current_id = document.getElementById('eventField').value;
   if ( event_current_id in event_map ) {
@@ -19,3 +41,15 @@ document.getElementById("eventField").addEventListener("submit", function(){
     document.getElementById('error').innerHTML='Le mot de pass est incorecte';
   }
 });
+
+document.getElementById('eventField').onkeydown = function(event){
+  if (event.key !== undefined) {
+    if(event.key === "Enter"){
+      document.getElementById("eventField").dispatchEvent(evt);
+    }
+  } else if (event.keyCode !== undefined) {
+    if(event.keyCode === 13){
+      document.getElementById("eventField").dispatchEvent(evt);
+    }
+  }
+};
