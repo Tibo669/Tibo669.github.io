0c63a9f34610e06023f8a97518d592dd8a3a6e69
bapti <lol@lol.fr>
2019-01-19 09:25:00 +0100

    Perso Assistant

--- src/js/data.js
+++ src/js/data.js
@@ -8,11 +8,23 @@ const event_json = `{
       ]
   },
   "6": {
-    "name": "Toto",
+    "name": "Thomas Goutte",
     "url": "event_martin_accsjo.html",
     "dialogue": [
-        "Bonjour, Je suis archéologue en chef et je m'occupe du site de fouille de  Roche-Cattin sur la commune de Bully.",
-        "Nous sommes sur la piste d’une Villae Rusticae, une maison de campagne romaine mais le temps presse !"
+        "Bonjour, vous êtes les fameux chasseurs de trésors, le Professeur Martin m’avait prévenu de votre venue.",
+        " Il s'excuse encore de ne pas être présent. Je vais me charger de vous expliquer la situation. ",
+        "Nous avons trouvé l’emplacement d’une Villae Rusticae à l’endroit où nous sommes.  Mais avant revoyons les bases.",
+        "La Gaule a été conquise par les romains en -52 avant Jésus Christ par un certain Jules César, je pense que vous en avez déjà entendu parler.",
+        "Une fois implantés dans les pays vaincus, les romains apportaient des éléments de leur civilisation, tels que les routes pavés, les amphithéâtres, leur monnaie et même leur religion.",
+        "Le village de Bully existait déjà au temps des romains et plusieurs éléments nous le prouvent.",
+        "Comme par exemple la présence de pavées appartenant à la route reliant Lugdunum-Lyon à Rodumna-Roanne,",
+        "des tombes, des pièces de monnaie, des tuiles et encore plus incroyable des thermes qui sont situées tout près d’ici.",
+        "Mais lors de cette découverte, nous avons trouvé un texte parlant d’un trésor enterré autour de la villae rusticae.",
+        "Et c’est là que vous intervenez, vous devez le trouver au plus vite, afin qu’il finisse dans un musée pour être étudié au nom de l’histoire et de l’archéologie.",
+        "N’hésitez pas à demander de l’aide au gens présents cependant restez sur vos gardes.",
+        "Au fait, je pense que vous le savez déjà mais les romains avaient pour habitude de cacher leurs objets de valeurs près d’un lieu symbolique,",
+        "comme un carrefour, un point d’eau ou une forêt. Je vous les ai recensé sur la carte."
+        
       ]
   }
 }`;
