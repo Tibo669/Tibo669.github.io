5bbc89a87e60bc55d83eb2b05d301cba796a312d
bapti <lol@lol.fr>
2019-01-19 09:46:25 +0100

    Perso

--- src/js/data.js
+++ src/js/data.js
@@ -8,11 +8,11 @@ const event_json = `{
       ]
   },
   "6": {
-    "name": "Thomas Goutte",
+    "name": "Thomas Goutte, assistant du professeur Martin",
     "url": "event_martin_accsjo.html",
     "dialogue": [
         "Bonjour, vous êtes les fameux chasseurs de trésors, le Professeur Martin m’avait prévenu de votre venue.",
-        " Il s'excuse encore de ne pas être présent. Je vais me charger de vous expliquer la situation. ",
+        "Il s'excuse encore de ne pas être présent. Je vais me charger de vous expliquer la situation. ",
         "Nous avons trouvé l’emplacement d’une Villae Rusticae à l’endroit où nous sommes.  Mais avant revoyons les bases.",
         "La Gaule a été conquise par les romains en -52 avant Jésus Christ par un certain Jules César, je pense que vous en avez déjà entendu parler.",
         "Une fois implantés dans les pays vaincus, les romains apportaient des éléments de leur civilisation, tels que les routes pavés, les amphithéâtres, leur monnaie et même leur religion.",
@@ -27,6 +27,73 @@ const event_json = `{
         
       ]
   }
+  "7": {
+    "name": "Tennesse Johnson, chasseur de trésor",
+    "url": "event_martin_accsjo.html",
+    "dialogue": [
+        "Ah donc voilà mes concurrents. Je vous imaginais plus grands, plus forts bref…",
+        "C’est gentil d’être venue essayer mais moi Tennessee Johnson, je tiens à vous dire un truc, je serai le premier à trouver ce maudit trésor et à moi le magot.",
+        "J’ai déjà des contacts pour le revendre. Allez, sur ce bonne journée et si vous voulez je vous signe un autographe."
+      ]
+  },
+  "8": {
+    "name": "Madame Petitejeanne, vieille voisine",
+    "url": "event_martin_accsjo.html",
+    "dialogue": [
+        "Bonjour, vous êtes nouveaux dans le coin, je ne vous ai jamais vu aussi et pourtant je connais tout le monde.",
+        "Vous faites partie de ses nouveaux chasseurs de trésor ? Ils ont débarqué depuis quelques jours et c’est à plus s’entendre.",
+        "Et vas-y que je creuse, vas-y que je déclame des textes en latin et encore je vous la fais courte.",
+        "Mais vous me semblez différent et bien sympathique les gones. Si ils étaient venu depuis le début, je leur aurais donné des informations moi mais qui fait attention à la vieille du quartier.",
+        "Sauf que moi le quartier, je le connais comme ma poche. Oui et la maison que vous voyez derrière c’est là que j’y suis née.",
+        "Tout change si vite maintenant, les seuls choses qui n’ont pas changé depuis toujours sont la mare et le noyer et les antiquités tels que le nez et le chapiteau.",
+        "Je ne sais pas si ça peut vous aider mais au moins c’est dit. Allez sur ce bonne journée. Je dois aller nourrir la télé et regarder minouche ou l’inverse."
+      ]
+  },
+  9": {
+    "name": "Jean Duval, vigneron",
+    "url": "event_martin_accsjo.html",
+    "dialogue": [
+        "Ça sera une bonne année ! Vous me direz, on n’a pas le choix, la nature fait son travail et nous on fait de notre mieux.",
+        "Mais désolé, je manque à tous mes devoirs, j’ai oublié de me présenter. Je m'appelle Jean Duval, viticulteur et vous qui vous êtes ?",
+        "Non me dites pas, vous chercher le fameux trésor, c’est ça ? Au moi ça ne m’étonne pas cette histoire de maison romaine !",
+        "Si vous saviez toutes les breloques  que j’ai pu trouver dans mes vignes, des pièces, des bouts de tuiles et même des bijoux c’est vous dire.",
+        "Si vous avez le temps une fois que vous avez fini votre enquête, passez au domaine, je pourrais vous les montrer.",
+        "Mais en attendant si vous voulez voir la pièce maîtresse de mes trouvailles, il va falloir répondre à quelques questions afin de vérifier vos connaissances en viticulture."
+      ]
+  },
+  "10": {
+    "name": "Marius Bedron, ramasseur de champignon",
+    "url": "event_martin_accsjo.html",
+    "dialogue": [
+        "Bonjour, comment allez-vous ? Moi ça va sauf que la cueillette n’est pas bonne.",
+        "Vous me direz que j’ai l’habitude, ce n’est pas un coin à champignon mais moi ça m’importe peu, c’est surtout pour me balader.",
+        "Alors je prends ma casquette et mon panier et je baguenaude. J’aime bien cet endroit allez savoir pourquoi.",
+        "J’avoue que je trouve plus de bout de tuile ou de poterie romaine que de champignons et en plus c’est moins digeste dans la salade mais c’est très agréable.",
+        "Vous êtes vous aussi à la recherche du trésor ? Moi j’ai bien essayé au début mais si il ressemble pas un cèpe je ne vais pas réussir à le trouver.",
+        "Le seul endroit où je le chercherai pas c’est la mare car c’est pas un bon endroit pour converser quelque chose.",
+        "Il y a un an j’avais perdu un bracelet la-bas et je l’ai retrouvé le mois dernier, bouffé par l’humidité, je vous dis pas. Bon sur ce je continu ma recherche et bonne chance pour la votre."
+      ]
+  },
+  "11": {
+    "name": "Jacques Pedroit, maire",
+    "url": "event_martin_accsjo.html",
+    "dialogue": [
+        "Bonjour, mes chers concitoyens et bienvenue dans la nouvelle attraction de Bully : Un parc thématique gallo-romain.",
+        "Il reste encore quelques détails à régler mais ça va être grandiose, j’ai hâte de découper le ruban d'inauguration.",
+        "Même si finalement on trouve rien, on fera des reproductions, c’est pas grave les gens ne sont pas obligés de savoir.",
+        "Il faut bien que ce projet se fasse, et je suis le seul capable de le mener à bien. D’ailleurs cela me prend un temps fou, tout le monde me sollicite, je suis indispensable.",
+        "Au fait l’archéologue m’a donné ce document, j’ai pas le temps de le traiter, je vous laisse vous en occupez, faites de votre mieux."
+      ]
+  },
+  "12": {
+    "name": "Hélène Vincier, historienne",
+    "url": "event_martin_accsjo.html",
+    "dialogue": [
+        "1848, la ruée vers l’or c’est exactement ce que vous faites, vous chercher un trésor. Nous les historiens nous sommes un peu comme vous ! ",
+        "On cherche, on cherche et puis on finit par trouver un élément historique. Je vous conseille cependant de vous méfier des apparences, il y a beaucoup de faux qui traine dans les environs.",
+        "Cependant seul un œil expert arrive à le déceler. D’ailleurs sauriez-vous capable de relier ces chapiteaux de colonnes avec leur période historique correspondante ?"
+      ]
+  },
 }`;
 
 let event_map = JSON.parse(event_json);
