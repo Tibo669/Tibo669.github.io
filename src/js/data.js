2fabe8b935d84f13ba3b8d3cb532a0db6d94e32b
Tibo669 <thib669@hotmail.fr>
2019-01-20 14:14:51 +0100

    Ajout du son pour l'assistant Amélioration dialogues

--- src/js/data.js
+++ src/js/data.js
@@ -11,41 +11,42 @@ const event_json = `{
     "name": "Thomas Goutte, Assistant du Pr. Martin",
     "url": "events/event_goutte_qsdkaz.html",
     "dialogue": [
+        "<i>Vous arrivez près d’un jeune homme affairé, tenant dans sa main une pierre. <br>Il est entouré de dessins et de schémas. <br>Il vous entend arriver et se retourne.</i>",
         "Bonjour, vous êtes les fameux chasseurs de trésors ! <br>Le Professeur Martin m’avait prévenu de votre visite.",
         "Il s'excuse encore de ne pas être présent. Je vais me charger de vous expliquer la situation. ",
         "Nous avons trouvé l’emplacement d’une <i>Villae Rusticae</i> à l’endroit où nous sommes. <br>Mais avant, revoyons les bases.",
         "La Gaule a été conquise par les romains en -52 avant Jésus Christ par un certain Jules César, je pense que vous en avez déjà entendu parler.",
-        "Une fois implantés dans les pays vaincus, les romains apportaient des éléments de leur civilisation, tels que les routes pavés, les amphithéâtres, leur monnaie et même leur religion.",
+        "Une fois implantés dans les pays vaincus, les romains apportaient des éléments de leur civilisation, tels que les routes pavées, les amphithéâtres, leur monnaie et même leur religion.",
         "Le village de Bully existait déjà au temps des romains et plusieurs éléments nous le prouvent.",
-        "Comme par exemple la présence de pavés appartenant à la route reliant Lugdunum-Lyon à Rodumna-Roanne,",
-        "des tombes, des pièces de monnaie, des tuiles et encore plus incroyable des thermes qui sont situées tout près d’ici.",
-        "Mais lors de cette découverte, nous avons trouvé un texte parlant d’un trésor enterré autour de la <i>Villae Rusticae</i>.",
+        "Comme par exemple la présence de pavés appartenant à la route reliant <i>'Lugdunum'</i> Lyon à <i>'Rodumna'</i> Roanne,",
+        "des tombes, des pièces de monnaie, des tuiles et encore plus incroyable : des thermes qui sont situés tout près d’ici.",
+        "Mais lors de cette découverte, nous avons trouvé un texte parlant <b>d’un trésor enterré</b> autour de la <i>Villae Rusticae</i>.",
         "Et c’est là que vous intervenez, vous devez le trouver au plus vite, afin qu’il finisse dans un musée pour être étudié au nom de l’histoire et de l’archéologie.",
-        "N’hésitez pas à demander de l’aide au gens présents cependant restez sur vos gardes.",
-        "Au fait, je pense que vous le savez déjà mais les romains avaient pour habitude de cacher leurs objets de valeurs près d’un lieu symbolique,",
-        "comme un carrefour, un point d’eau ou une forêt. Je vous les ai recensé sur la carte."
+        "N’hésitez pas à demander de l’aide au gens présents. <br>Cependant, restez sur vos gardes !",
+        "Au fait, je pense que vous le saviez déjà, mais les romains adoraient cacher leurs objets de valeurs près d’un lieu symbolique, comme un carrefour ou un point d’eau. <br>Je vous les ai recensés sur la carte.",
+        "<i>Le jeune homme vous regarde une dernière fois et se replonge dans l’étude des documents devant lui.</i>"
       ]
   },
   "7": {
     "name": "Tennesse Johnson, Chasseur de trésor",
     "url": "events/event_jonson_qsdjan.html",
     "dialogue": [
-        "Ah ! Donc voilà mes concurrents. <br>Je vous imaginais plus grands, plus forts bref…",
-        "C’est gentil d’être venue essayer mais moi, Tennessee Johnson, je tiens à vous dire un truc, je serai le premier à trouver ce maudit trésor et à moi le magot !",
-        "J’ai déjà des contacts pour le revendre. Allez, sur ce, bonne journée et si vous voulez je peux vous signer un autographe."
+        "Ah ! Donc, voilà, mes concurrents. <br>Je vous imaginais plus grands, plus forts bref…",
+        "C’est gentil d’être venu essayer mais moi, <b>Tennessee Johnson</b>, je tiens à vous dire un truc : <br>Je serai le premier à trouver ce maudit trésor et à moi le magot !",
+        "J’ai déjà des contacts pour le revendre. Allez ! Bonne journée ! <br>Mais si vous voulez, je peux vous signer un autographe !"
       ]
   },
   "8": {
     "name": "Madame Petitejeanne, Vieille voisine",
     "url": "events/event_petitejeanne_podksl.html",
     "dialogue": [
-        "Bonjour, vous êtes nouveaux dans le coin, je ne vous ai jamais vu ici et pourtant je connais tout le monde.",
-        "Vous faites partie de ses nouveaux chasseurs de trésor ? <br>Ils ont débarqué depuis quelques jours et c’est à plus s’entendre.",
-        "Et vas-y que je creuse, vas-y que je déclame des textes en latin et encore je vous la fais courte.",
-        "Mais vous me semblez différent et bien sympathique les gones. Si ils étaient venu depuis le début, je leur aurais donné des informations, moi ! <br>Mais qui fait attention à la vieille du quartier ?",
-        "Sauf que moi, le quartier, je le connais comme ma poche. Oui et la maison que vous voyez derrière, c’est là que j’y suis née.",
-        "Tout change si vite maintenant, les seuls choses qui n’ont pas changées depuis toujours sont la mare et le noyer et les antiquités tels que le nez et le chapiteau.",
-        "Je ne sais pas si ça peut vous aider mais au moins c’est dit. <br>Allez sur ce, bonne journée. Je dois aller nourrir la télé et regarder minouche ou l’inverse."
+        "Bonjour, vous êtes nouveaux dans le coin. Je ne vous ai jamais vu ici et pourtant, je connais tout le monde.",
+        "Vous faites parti de ces nouveaux chasseurs de trésor ? <br>Ils ont débarqué depuis quelques jours et c’est à plus s’entendre.",
+        "Et vas-y que je creuse, vas-y que je déclame des textes en latin et encore, je vous la fais courte...",
+        "Mais vous me semblez différents et bien sympathiques les gones. Si ils étaient venus depuis le début, je leur aurai donné des informations, moi ! <br>Mais qui fait attention à la vieille du quartier ?",
+        "Sauf que moi, le quartier, je le connais comme ma poche. Oui et la maison que vous voyez derrière moi, c’est là que j’y suis née.",
+        "Tout change si vite maintenant, les seules choses qui n’ont pas changées depuis toujours sont <b>la mare</b>, <b>le noyer</b> et les antiquités tels que <b>le nez</b> et <b>le chapiteau</b>.",
+        "Je ne sais pas si ça peut vous aider, mais au moins, c’est dit. <br>Allez sur ce, bonne journée. Je dois aller nourrir la télé et regarder minouche, ou l’inverse."
       ]
   },
   "9": {
@@ -53,11 +54,11 @@ const event_json = `{
     "url": "events/event_duvalon_ghsksl.html",
     "dialogue": [
         "Ça sera une bonne année ! <br>Vous me direz, on n’a pas le choix, la nature fait son travail et nous on fait de notre mieux.",
-        "Mais désolé, je manque à tous mes devoirs, j’ai oublié de me présenter. <br>Je m'appelle Jean Duvalon, viticulteur et vous qui vous êtes ?",
-        "Non, ne me dites pas, vous chercher le fameux trésor, c’est ça ? <br>Oh moi ça ne m’étonne pas cette histoire de maison romaine !",
-        "Si vous saviez toutes les breloques  que j’ai pu trouver dans mes vignes, des pièces, des bouts de tuiles et même des bijoux c’est vous dire.",
-        "Si vous avez le temps une fois que vous avez fini votre enquête, passez au domaine, je pourrais vous les montrer.",
-        "Mais en attendant si vous voulez voir la pièce maîtresse de mes trouvailles, il va falloir répondre à quelques questions afin de vérifier vos connaissances en viticulture."
+        "Mais désolé, je manque à tous mes devoirs, j’ai oublié de me présenter. <br>Je m'appelle Jean Duvalon, viticulteur et vous qui êtes-vous ?",
+        "Non, ne me dites pas, vous chercher le fameux trésor, c’est ça ? <br>Oh moi, ça ne m’étonne pas cette histoire de maison romaine !",
+        "Si vous saviez toutes les breloques  que j’ai pu trouver dans mes vignes, des pièces, des bouts de tuiles et même des bijoux, c’est vous dire !",
+        "Si vous avez le temps une fois que vous avez fini votre enquête, passez au domaine, je pourrai vous les montrer.",
+        "Mais en attendant, si vous voulez voir la pièce maîtresse de mes trouvailles, <br>il va falloir répondre à quelques questions afin de vérifier vos connaissances en viticulture..."
       ]
   },
   "10": {
@@ -65,12 +66,12 @@ const event_json = `{
     "url": "events/event_bedron_lskfos.html",
     "dialogue": [
         "Bonjour, comment allez-vous ? <br>Moi ça va, sauf que la cueillette n’est pas bonne.",
-        "Vous me direz que j’ai l’habitude, ce n’est pas un coin à champignon ! <br>Mais moi ça m’importe peu, c’est surtout pour me balader.",
-        "Alors je prends ma casquette et mon panier et je baguenaude. <br>J’aime bien cet endroit allez savoir pourquoi.",
-        "J’avoue que je trouve plus de bout de tuile ou de poterie romaine que de champignons. <br>C’est moins digeste dans la salade mais c’est très agréable.",
-        "Vous êtes vous aussi à la recherche du trésor ? <br>Moi j’ai bien essayé au début mais si il ne ressemble pas un cèpe je ne vais pas réussir à le trouver.",
-        "Le seul endroit où je ne le chercherai pas c’est la mare car c’est pas un bon endroit pour converser quelque chose.",
-        "Il y a un an j’avais perdu un bracelet la-bas et je l’ai retrouvé le mois dernier, bouffé par l’humidité, je vous dis pas. <br>Bon sur ce, je continue ma recherche et bonne chance pour la votre !"
+        "Vous me direz que j’ai l’habitude, ce n’est pas un coin à champignon ! <br>Mais moi, ça m’importe peu, c’est surtout pour me balader.",
+        "Alors je prends ma casquette, mon panier et je baguenaude. <br>J’aime bien cet endroit, allez savoir pourquoi...",
+        "J’avoue que je trouve plus de bouts de tuiles ou de poteries romaines que de champignons. <br>C’est moins digeste dans la salade mais c’est très agréable !",
+        "Vous êtes vous aussi à la recherche du trésor ? <br>Moi j’ai bien essayé, au début, mais si il ne ressemble pas à un cèpe, je ne vais pas réussir à le trouver.",
+        "Le seul endroit où je ne le chercherai pas, c’est <b>la mare</b>. <br>Ce n'est pas un bon endroit pour conserver quelque chose !",
+        "Il y a un an, j’ai perdu un bracelet la-bas et je l’ai retrouvé le mois dernier, bouffé par l’humidité, je ne vous dis pas ! <br>Bon, je continue ma recherche et bonne chance pour la vôtre !"
       ]
   },
   "11": {
@@ -79,8 +80,8 @@ const event_json = `{
     "dialogue": [
         "Bonjour, mes chers concitoyens et bienvenue dans la nouvelle attraction de Bully : <br>Un parc thématique gallo-romain.",
         "Il reste encore quelques détails à régler mais ça va être grandiose ! <br>J’ai déjà hâte de découper le ruban d'inauguration.",
-        "Même si finalement on ne trouve rien, on fera des reproductions. <br>Ce n'est pas grave les gens ne sont pas obligés de le savoir.",
-        "Il faut bien que ce projet se fasse, et je suis le seul capable de le mener à bien. <br>D’ailleurs cela me prend un temps fou, tout le monde me sollicite, je suis indispensable.",
+        "Même si on ne trouve rien, on fera des reproductions. <br>Ce n'est pas grave, les gens ne sont pas obligés de le savoir.",
+        "Il faut bien que ce projet se fasse, et je suis le seul capable de le mener à bien. <br>D’ailleurs, cela me prend un temps fou, tout le monde me sollicite, je suis indispensable !",
         "Au fait, l’archéologue m’a donné ce document, je n’ai pas le temps de le traiter, je vous laisse vous en occupez, faites de votre mieux."
       ]
   },
@@ -88,9 +89,9 @@ const event_json = `{
     "name": "Hélène Vincier, Historienne",
     "url": "events/event_vincier_roeksn.html",
     "dialogue": [
-        "1848, la ruée vers l’or, c’est exactement ce que vous faites, vous chercher un trésor. <br>Nous, les historiens, nous sommes un peu comme vous ! ",
-        "On cherche, on cherche et puis on fini par trouver un élément historique. <br>Je vous conseille cependant de vous méfier des apparences, il y a beaucoup de faux qui traine dans les environs.",
-        "Cependant seul un oeil expert arrive à le déceler. <br>D’ailleurs, seriez-vous capable de relier ces chapiteaux de colonnes avec leur période historique correspondante ?"
+        "1848, la ruée vers l’or, c’est exactement ce que vous faites, vous cherchez un trésor. <br>Nous, les historiens, nous sommes un peu comme vous ! ",
+        "On cherche, on cherche et puis on fini par trouver un élément historique. <br>Je vous conseille cependant de vous méfier des apparences, il y a beaucoup de faux qui trainent dans les environs.",
+        "Cependant, seul un oeil expert arrive à le déceler. <br>D’ailleurs, seriez-vous capable de relier ces chapiteaux de colonnes avec leur période historique correspondante ?"
       ]
   },
   "13": {
--- src/js/event.js
+++ src/js/event.js
@@ -4,11 +4,25 @@ dialogue_list = event_map[personage]['dialogue'];
 
 document.getElementById("dialogueNameButton").innerHTML = event_map[personage]['name'];
 dialogue_idx = 1;
-document.getElementById("dialogueButton").innerHTML = dialogue_list[0];
+dialogueButton = document.getElementById("dialogueButton");
+dialogueButton.innerHTML = dialogue_list[0];
+// Intro du personnage
+document.getElementById("eventAudioIntro").play();
 
 document.getElementById('dialogueButton').addEventListener('click',function(){
+  // Sortie du personnage
+  if ( dialogue_idx === dialogue_list.length - 1 ) {
+    dialogueButton.style.backgroundImage = "url(../../img/dialogue_button_no_arrow.png)";
+    document.getElementById("eventAudioDialogue").pause();
+    document.getElementById("eventAudioFin").play();
+  }
+  else if ( dialogue_idx < dialogue_list.length - 1 ) {
+    document.getElementById("eventAudioIntro").pause();
+    document.getElementById("eventAudioDialogue").play();
+  }
+  // Avancée dans le dialogue
   if ( dialogue_idx < dialogue_list.length ) {
-    document.getElementById("dialogueButton").innerHTML = dialogue_list[dialogue_idx];
+    dialogueButton.innerHTML = dialogue_list[dialogue_idx];
     dialogue_idx ++;
   }
 },false);
