07affd895310f60178eb11cf2be850aaaa5a8213
tb6a210n <thibault-externe.besacier@enedis.fr>
2019-01-23 16:35:03 +0100

    Gestion du chrono de fin et du temps de partie dépassé

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 let minutes, seconds; // variables for time units
 let targetDate;
 
+const gameDuration = 1000*3600;
+
 let target_date_stored = sessionStorage.getItem('target_date');
 
 if (target_date_stored !== null) {
@@ -9,7 +11,7 @@ if (target_date_stored !== null) {
   targetDate = targetDate.getTime();
 }
 else {
-  targetDate = new Date().getTime() + (1000*3600); // set the countdown date
+  targetDate = new Date().getTime() + gameDuration; // set the countdown date
   sessionStorage.setItem('target_date', targetDate.toString());
 }
 
@@ -26,16 +28,53 @@ function getCountdown(){
   targetDate = targetDate.getTime();
   // find the amount of "seconds" between now and target
   let currentDate = new Date().getTime();
-  let seconds_left = (targetDate - currentDate) / 1000;
-  if (seconds_left <= 0) {
-    seconds_left = 0;
+
+  let countdownColor = "";
+  if ( targetDate >= currentDate ) {
+    let seconds_left = (targetDate - currentDate) / 1000;
+    if (seconds_left <= 0) {
+      seconds_left = 0;
+    }
+    minutes = pad(parseInt(seconds_left / 60));
+    seconds = pad(parseInt(seconds_left % 60));
+    if (parseInt(minutes) < 5) {
+      countdownColor = " style='color: darkorange'";
+    }
+  }
+  else {
+    let seconds_left = (currentDate - targetDate) / 1000;
+    minutes = pad(parseInt(seconds_left / 60));
+    seconds = pad(parseInt(seconds_left % 60));
+    countdownColor = " style='color: darkred'";
   }
 
-  minutes = pad( parseInt( seconds_left / 60) );
-  seconds = pad( parseInt( seconds_left % 60 ) );
+  // format countdown string + set tag value
+  countdown.innerHTML = "<span" + countdownColor + ">" + minutes + "</span><span" + countdownColor + ">" + seconds + "</span>";
+}
+
+function getFinalCountdown(){
+  target_date_stored = sessionStorage.getItem('target_date');
+  targetDate = new Date(0);
+  targetDate.setUTCMilliseconds(parseInt(target_date_stored));
+  targetDate = targetDate.getTime();
+  // find the amount of "seconds" between now and target
+  let currentDate = new Date().getTime();
+
+  let countdownColor = "";
+  // On vérifie si on est dans les temps ou pas
+  if ( targetDate >= currentDate ) {
+    let secondsSpend = (gameDuration - (targetDate - currentDate)) / 1000;
+    minutes = pad(parseInt(secondsSpend / 60));
+    seconds = pad(parseInt(secondsSpend % 60));
+  }
+  else {
+    let secondsSpend = (gameDuration + (currentDate - targetDate)) / 1000;
+    minutes = pad(parseInt(secondsSpend / 60));
+    seconds = pad(parseInt(secondsSpend % 60));
+  }
 
   // format countdown string + set tag value
-  countdown.innerHTML = "<span>" + minutes + "</span><span>" + seconds + "</span>";
+  countdown.innerHTML = "<span" + countdownColor + ">" + minutes + "</span><span" + countdownColor + ">" + seconds + "</span>";
 }
 
 function pad(n) {
