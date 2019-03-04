0672a0fecfeee41637e250f5709b2797e3c128f3
Tibo669 <thib669@hotmail.fr>
2019-03-04 22:59:12 +0100

    Modification cadenas clic

--- src/js/events/event_cadenas.js
+++ src/js/events/event_cadenas.js
@@ -5,85 +5,46 @@ function createCadenas() {
   document.getElementById("lock-plate").style.display = "block";
 }
 
-$(function(){
 
-  var comboArray = [0, 0, 0, 0];
-  var combination = [1, 1, 5, 1];
+var comboArray = [0, 0, 0, 0];
+var combination = [1, 1, 5, 1];
 
-  var gridIncrement = $( ".lock-dial ul" ).css('line-height').replace('px', '')/2;
-  var numNums = $( ".lock-dial:eq(0) ul li" ).length;
-  var halfHeight = gridIncrement*numNums;
-  var initTop = -(halfHeight-gridIncrement);
+var gridIncrement = $( ".lock-dial ul" ).css('line-height').replace('px', '')/2;
+var numNums = $( ".lock-dial:eq(0) ul li" ).length;
+var halfHeight = gridIncrement*numNums;
+var initTop = -(halfHeight-gridIncrement);
 
-  $( ".lock-dial ul" ).css('top', initTop);
+let lockDial = $( ".lock-dial ul" );
 
-  $( ".lock-dial ul" ).draggable({
-    grid: [ 0, gridIncrement ],
-    axis: 'y',
-    drag: function(){
-      var dragDir = $(this).css('top').replace('px', '') < initTop ? "up" : "down";
-
-      if(dragDir == "up"){
-        var curNum = parseInt($(this).find('li:last-child').text()) + 1;
-        if(curNum < 10){
-          $(this).append('<li>'+curNum+'</li>');
-        }else{
-          $(this).append('<li>0</li>');
-        };
-      }else{
-        var curNum = parseInt($(this).find('li:first-child').text()) - 1;
-        var thisTop = parseInt($(this).css('margin-top').replace('px', ''));
-
-        $(this).css({
-          marginTop: thisTop-(gridIncrement*2)
-        });
-
-        if(curNum > -1){
-          $(this).prepend('<li>'+curNum+'</li>');
-        }else{
-          $(this).prepend('<li>9</li>');
-        };
-      };
-    },
-    stop: function(){
-
-      //MATHS
-      var negOrPos = $(this).css('margin-top').replace('px', '') > 0 ? 1 : -1;
-      var thisTopTotal = parseInt($(this).css('top').replace('px', '')) + Math.abs(initTop);
-      var marginMinified = parseInt(Math.abs($(this).css('margin-top').replace('px', ''))) - thisTopTotal;
-      var numIncs = Math.floor(marginMinified/(halfHeight*2));
-      var totalDif = numIncs*(halfHeight*2);
-      var topTen = (marginMinified - totalDif)*negOrPos;
-      var activeIndex = Math.abs(topTen/(gridIncrement*2)) + (halfHeight/(gridIncrement*2));
-
-      $(this).attr("data-combo-num", $(this).find('li').eq(activeIndex).text()).css({
-        top: -315,
-        marginTop: topTen
-      }).find('li').slice(20).remove();
-
-      for(var i=0; i<$( ".lock-dial ul" ).length; i++){
-        comboArray[i] = $( ".lock-dial ul:eq("+i+")" ).attr("data-combo-num");
-      }
-
-
-      if(comboArray == ""+combination){
-        $('.lock-dial ul').draggable('disable');
-        $('#lock-wrapper').addClass("unlocked");
-        $('.lock-dial').each(function(){
-          var $this = $(this);
-          $this.find('ul').delay(400).css('color', '#0f0').fadeOut(function(){
-            $this.animate({
-              marginTop: 150
-            }, function(){
-              $this.fadeOut(function(){
-                sessionStorage.setItem('current_personage', tresorID);
-                location.href = '../' + event_map[tresorID]["url"];
-              });
-            });
-          });
-        });
-      }
+lockDial.on("click", function(){
+  let middleValue = 0;
+  $(this).find('li').each(function(){
+    let currentValue = parseInt($(this).text());
+    currentValue = (currentValue + 1) % 10;
+    $(this)[0].innerText = currentValue.toString();
+    if ( $(this).attr("class") === "middle" ) {
+      middleValue = currentValue;
     }
   });
 
-})
+  comboArray[parseInt($(this).attr("data-combo-num"))] = middleValue;
+
+  if(comboArray[0] === combination[0] && comboArray[1] === combination[1] && comboArray[2] === combination[2] && comboArray[3] === combination[3]){
+    $('#lock-wrapper').addClass("unlocked");
+    $('.lock-dial').each(function(){
+      var $this = $(this);
+      $this.find('ul').delay(400).css('color', '#0f0').fadeOut(function(){
+        $this.animate({
+          marginTop: 150
+        }, function(){
+          $this.fadeOut(function(){
+            sessionStorage.setItem('current_personage', tresorID);
+            location.href = '../' + event_map[tresorID]["url"];
+          });
+        });
+      });
+    });
+  }
+});
+
+
